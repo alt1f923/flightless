35478b87bca04a15abc5729db78e244865fc4639
Duncan Fryer <35885610+alt1f923@users.noreply.github.com>
2019-11-18 18:31:43 +1300

    connection code
    finished connection code

--- Python - discord.py 2019/main.py
+++ Python - discord.py 2019/main.py
@@ -3,52 +3,54 @@ import asyncio
 import sys
 
 class Flightless(discord.Client):
+    def __init__(self, token):
+        super().__init__()
+        self.token = token
+
     async def on_ready(self):
         print(
             f"Ready as: {self.user.name}",
             f"Running Discord.py v{discord.__version__}",
             f"Serving {len(self.guilds)} guilds with a total of {len(self.users)} users",
-            sep='\n'
-        )
-        
+            sep='\n')
+
+    async def start(self):
+        print("Logging in...")
+        try:
+            await self.login(self.token, bot=True)
+            print("Connecting...")
+            await self.connect(reconnect=True)
+        except discord.errors.LoginFailure:
+            # Invalid token causes LoginFailure
+            print("Invalid token provided", file=sys.stderr)
+        except discord.errors.HTTPException as e:
+            # HTTP error code raised
+            print(f"HTTP request operation failed, status code: {e.status}", file=sys.stderr)
+        except discord.errors.GatewayNotFound:
+            # Unable to reach Discords API, the API being down will probably also mean no one will be online on the client to complain about the bot :^)
+            print("Cannot reach Discord gateway, possible Discord API outage", file=sys.stderr)
+        except discord.errors.ConnectionClosed:
+            # Connection terminated after it was established, probably caused by internet dropping out, reconnect should take care of this
+            print("The websocket connection has been terminated", file=sys.stderr)
+
+    async def disconnect(self):
+        # Logout
+        await self.logout()
+        print("Disconnected")
+
+    def run(self):
+        # Create the loop
+        loop = asyncio.get_event_loop()
+        try:
+            # Connect to Discord using the token stored as one of the system's environment variables
+            loop.run_until_complete(self.start())
+        except KeyboardInterrupt:
+            # If a keyboard interupt is sent to the console, send the bot into invisible mode and log it out
+            loop.run_until_complete(self.disconnect())
+        finally:
+            # Close the loop
+            loop.close()
     
-client = Flightless()
-
-
-async def connect(token):
-    print("Logging in...")
-    try:
-        await client.login(token, bot=True)
-        print("Connecting...")
-        await client.connect(reconnect=True)
-    except discord.errors.LoginFailure:
-        print("Invalid token provided", file=sys.stderr)
-        await client.close()
-    except discord.errors.HTTPException as e:
-        print(f"HTTP request operation failed, status code: {e.status}", file=sys.stderr)
-    except discord.errors.GatewayNotFound:
-        print("Cannot reach Discord gateway, possible Discord API outage", file=sys.stderr)
-    except discord.errors.ConnectionClosed:
-        print("The websocket connection has been terminated", file=sys.stderr)
-
-async def disconnect():
-    # Logout
-    await client.logout()
-    print("Disconnected")
-
-def run(token):
-    # Create the loop
-    loop = asyncio.get_event_loop()
-    try:
-    # Connect to Discord using the token stored as one of the system's environment variables
-        loop.run_until_complete(connect(token))
-    except KeyboardInterrupt:
-    # If a keyboard interupt is sent to the console, send the bot into invisible mode and log it out
-        loop.run_until_complete(disconnect())
-    finally:
-        # Close the loop
-        loop.close()
-
         
 
     
@@ -57,7 +59,8 @@ def main():
         print(f"Usage: python {sys.argv[0]} token", file=sys.stderr)
     else:
         # Run the bot with the token provided from the arguments
-        run(sys.argv[1])
+        client = Flightless(sys.argv[1])
+        client.run()
 
 
 if __name__ == "__main__":
