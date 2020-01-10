bf234a46fcba0a1970de545b6d15b0461a97e175
Duncan Fryer <35885610+alt1f923@users.noreply.github.com>
2020-01-10 16:25:44 +1300

    top command and some bug fixes

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,12 @@ import re                               # Regex for parsing text for command and
 import shelve                           # For persistency regarding tags
 import requests                         # For checking if urls to images exist for tags
 from googletrans import Translator      # Free (unofficial) google translate API https://github.com/ssut/py-googletrans
+import matplotlib.pyplot as plt
+from bisect import bisect
+import numpy as np
+
+def plot_colour(d):
+    return [d.r/255, d.g/255, d.b/255]
 
 class Tag():
     def __init__(self, name, owner=165765321268002816, reply=None, url=None):
@@ -18,13 +24,14 @@ class Tag():
     def __str__(self):
         return f"Command: {self.name}\nOwner id: {self.owner}\nCreated: {self.date.strftime('%y/%m/%d %H:%M:%S')}"
 
+
 class Flightless(discord.Client):
     def __init__(self, token):
         super().__init__()
         # The reason token is set here is so I can disconnect the bot and reconnect it without restarting the code or carrying the token around as a global
         self.token               = token
         # Regex following the format of "f/word word word word"
-        self.message_parser      = re.compile(r"^f/([a-zA-Z]+) *([a-zA-Z]*) *([a-zA-Z]*) *(.*)$")
+        self.message_parser      = re.compile(r"^f/(\w+) *(\w*) *(\w*) *(.*)$")
         # Regex following the format of "https://www.website.com/image.png"
         # TODO: []() exclusion
         self.image_url_parser    = re.compile(r"^(.*)(https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|jpeg|webp|gif|png))( ?.*)$")
@@ -40,28 +47,66 @@ class Flightless(discord.Client):
                                     "time": self.time_command,
                                     "translate": self.translate_command,
                                     "game": self.game_server_command}
-        self.translator = Translator()
+        # Dictionary for storing the score data for the top command
+        self.guilds_score        = {}
+        # Translator from googletrans, used for translate command 
+        self.translator          = Translator()
+        # Ready, can use bot once this is True
+        self.ready               = False
 
     async def on_ready(self):
+
         print(
             f"Ready as: {self.user.name}",
             f"Running Discord.py v{discord.__version__}",
             f"Serving {len(self.guilds)} guilds with a total of {len(self.users)} users",
             sep='\n')
 
+        # Counting messages by user by server for the top command
+        for guild in self.guilds[1:2]:
+            users = {}
+            total = 0
+
+            for channel in guild.text_channels:
+                try:
+                    async for message in channel.history(limit=None):
+                        if (author := message.author) in guild.members:
+                            if not author.bot:
+                                if (author_id := author.id) not in users.keys():
+                                    users[author_id] = [1, author.name, author.colour, author.roles[-1].id]
+                                else:
+                                    users[author_id][0] += 1
+                                total += 1
+                except discord.errors.Forbidden:
+                    continue
+            self.guilds_score[guild.id] = [total, users]
+
+        await self.change_presence(status=discord.Status.online)
+        self.ready = True
+
     async def on_message(self, message):
-        if message.guild.id == 198337653412855808: # So that no server but mine will get interactions with my bot while testing --THIS IS TEMPORARY--
-            if not message.author.bot:
-                # TODO: Add blacklist check here
-                if parsed_message := self.message_parser.match(message.content):
-                    parsed_message = parsed_message.groups()
-                    command = parsed_message[0]
-                    if self.alias_exists(command):
-                        command = self.alias(command)
-                        if command_tag := self.bc.get(command, False):
-                            await self.send_tag(command_tag, message.channel)
+        if self.ready:
+            if message.guild.id == 198337653412855808: # So that no server but mine will get interactions with my bot while testing --THIS IS TEMPORARY--
+                if not message.author.bot:
+                    # TODO: Add blacklist check here
+                    if parsed_message := self.message_parser.match(message.content):
+                        parsed_message = parsed_message.groups()
+                        command = parsed_message[0]
+                        if self.alias_exists(command):
+                            command = self.alias(command)
+                            if command_tag := self.bc.get(command, False):
+                                await self.send_tag(command_tag, message.channel)
+                            else:
+                                await self.nbc[command](parsed_message, message)
+
+                    if message.guild.id not in self.guilds_score.keys():
+                        self.guilds_score[message.guild.id] = [1, {message.author.id: [1, message.author.name, message.author.colour, message.author.roles[-1].id]}]
+                    else:
+                        self.guilds_score[message.guild.id][0] += 1
+                        if message.author.id not in self.guilds_score[message.guild.id][1].keys():
+                            self.guilds_score[message.guild.id][1][message.author.id][0] = 1
                         else:
-                            await self.nbc[command](parsed_message, message)
+                            self.guilds_score[message.guild.id][1][message.author.id][0] += 1
 
     def seperate_url(self, content):
         url = None
@@ -117,7 +162,7 @@ class Flightless(discord.Client):
             if self.new_tag(owner=message.author.id, name=(name := input[2].lower()), reply=input[3]): # owner, name, reply
                 await self.send_tag(self.bc[name], message.channel)
             else:
-                await self.send_embed(message.channel, content="Tag could not be created.\nPlease make sure it's name is not already in use.")
+                await self.send_embed(message.channel, content="Tag could not be created.\nPlease make sure its name is not already in use.")
         elif instruction == "edit":
             if self.edit_tag((name := input[2].lower()), message.author.id, input[3]): # name, user, new_reply
                 await self.send_tag(self.bc[name], message.channel)
@@ -141,7 +186,74 @@ class Flightless(discord.Client):
         await self.send_embed(message.channel, title=f"{self.user.name.capitalize()}' reserved Aliases for Commands/Tags", fields=fields) # Hardcoded ' instead of 's since Flightless ends with a 's'  
 
     async def top_command(self, input, message):
-        await self.niy_command("Top", message.channel)
+        users = self.guilds_score[message.guild.id]
+        total = users[0]
+        users = users[1]
+
+        values = {}
+        labels = {}
+        colours = {}
+        other_users = 0
+
+        for user in users.keys():
+            score, name, colour, role = users[user]
+            if (percentage := score/total) < 0.005:
+                other_users += score 
+            else: 
+                if role not in values.keys():
+                    values[role] = [score]
+                    labels[role] = [f"{name} {(percentage * 100):.2f}%"]
+                    colours[role] = [plot_colour(colour)]
+                else:
+                    values[role].insert((index := bisect(values[role], score)), score)
+                    labels[role].insert(index, f"{name} {(percentage * 100):.2f}%")
+                    colours[role].insert(index, plot_colour(colour))
+        x = []
+        y = []
+        z = []
+        
+        for key in values.keys():
+            x += values[key]
+            label = ""
+            if len(labels[key]) > 1:
+                label += f"{message.guild.get_role(key).name}\n\n"
+            for item in labels[key]:
+                label += f"{item}\n"
+            y.append([label.strip(), len(labels[key])])
+            z += colours[key]
+
+        if other_users:
+            x.append(other_users)
+            y.append([f"Other users {(other_users/total * 100):.2f}%", 1])
+            z.append([0, 0, 0])
+
+        ax = plt.subplots(figsize=(10, 9), dpi=180, subplot_kw={"aspect" : "equal"})[1]
+
+        wedges = ax.pie(x, colors=z, wedgeprops={"width" : 0.5, "edgecolor":"0", 'linewidth': 1, 'linestyle': 'solid', 'antialiased': True}, startangle=0)[0]
+
+        kw = {"arrowprops" : {"arrowstyle" : "-"}, "bbox" : {"boxstyle" : "square,pad=0.6", "fc" : "w", "ec" : "k", "lw" : 0.72}, "zorder" : 0, "va" : "center"}
+
+        labels = y.copy()
+        count = 1
+        for p in wedges:
+            ang = (p.theta2 - p.theta1)/2. + p.theta1
+            y = np.sin(np.deg2rad(ang))
+            x = np.cos(np.deg2rad(ang))
+            horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+            connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+            kw["arrowprops"].update({"connectionstyle": connectionstyle})
+            if count == ((labels[0][1]) // 2) + 1:
+                ax.annotate(labels[0][0], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y), horizontalalignment = horizontalalignment, **kw)
+                count = -(labels[0][1] - count) + 1
+                del labels[0]
+            else:
+                count += 1
+
+        ax.set_title(f"{message.guild.name.capitalize()}")
+        plt.savefig(f"images/{message.guild.id}.png", bbox_inches="tight")
+
+        await message.channel.send(content=message.author.mention, 
+                                    file=discord.File(fp=open(f"images/{message.guild.id}.png", "rb"), filename=f"{message.guild.name}.png"))
 
     async def time_command(self, input, message):
         await self.niy_command("Time", message.channel)
@@ -206,7 +318,6 @@ class Flightless(discord.Client):
                     tag.reply = reply
                 else:
                     tag.reply = None
-                tag.reply = new_reply
                 self.save_tags()
                 return True
         return False
@@ -250,7 +361,7 @@ class Flightless(discord.Client):
             return False
 
     def alias(self, name):
-        """Dictionary lookup to find a command from it's alias, if found it will return the first value of alias (commands name) otherwise will return base command name"""
+        """Dictionary lookup to find a command from its alias, if found it will return the first value of alias (commands name) otherwise will return base command name"""
         return self.aliases.get(name, name)
 
     async def start(self):
@@ -294,7 +405,7 @@ class Flightless(discord.Client):
             # Connect to Discord using the token stored as one of the system's environment variables
             loop.run_until_complete(self.start())
         except KeyboardInterrupt:
-            # If a keyboard interupt is sent to the console, send the bot into invisible mode and log it out
+            # If a keyboard interupt is sent to the console log the bot out
             loop.run_until_complete(self.disconnect())
         finally:
             # Close the loop
